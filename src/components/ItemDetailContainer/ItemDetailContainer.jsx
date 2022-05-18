1c63d20b9bad64bb4c4b15f51d24df6f90ccf479
unknown <sebafantini@yahoo.com>
2022-05-18 00:10:25 -0300

    desafio

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemDetail from '../ItemDetail/ItemDetail'
+
+export default function ItemDetailContainer() {
+  return (
+    <div>
+    <h3>Este es el ItemDetailContainer </h3>
+    <ItemDetail/>
+    </div>
+    
+  )
+}
