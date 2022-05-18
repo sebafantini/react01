5fab7bae9db7f8d9dd6c9948d2287980eb950079
unknown <sebafantini@yahoo.com>
2022-05-18 00:57:53 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Formulario from './components/Formulario/Formulario';
 
 import Header from './components/Header';
 import Item from './components/Item/Item';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemList from './components/ItemList/ItemList';
 
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
@@ -49,7 +50,7 @@ function App() {
             <Titulo titulo={tit} subTit={subTit}/>      
             <FormContainer/>      
             <ItemListContainer greeting ='Hola, soy una prop'/>
-            
+            <ItemDetailContainer/>
         
       </div>
     
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import { useEffect, useState } from "react"
+import {Button} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
+import {InputGroup} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+
+const productos = [
+    { id: '1', title: 'Curry de Pollo', price: 1200, pictureUrl:'curry_pollo.JPG'},
+  ]
+
+const getFetch = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve( productos )        
+    }, 2000)
+})
 
 export default function ItemDetail() {
-  return (
-    <div>ItemDetail</div>
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getFetch  // fetch llamada a una api  
+        .then(respuesta=> setProductos(respuesta))
+        .catch((err)=> console.log(err))
+        .finally(()=>setLoading(false))     
+    }, [])
+    
+    console.log(productos)
+    
+    return (
+    <div>
+           { loading ? 
+                <h2>Cargando...</h2> 
+                : 
+                productos.map((prod) =>  
+                                
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={prod.pictureUrl} />
+                        <Card.Body>
+                            <Card.Title>{prod.title}</Card.Title>
+                            <Card.Text>
+                            ID_Producto: {prod.id}
+                            </Card.Text>
+
+                            <Card.Text>
+                            Precio: ${prod.price}
+                            </Card.Text>
+                                                                
+                            <Button variant="primary" size="lg" >
+                                Detalle
+                            </Button>                
+                        </Card.Body>
+                    </Card>
+                
+                )
+                        
+            }
+
+    </div>
   )
 }
