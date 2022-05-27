5fe5635fda8d8431b39c45ad5d038fdc19cdfbd9
unknown <sebafantini@yahoo.com>
2022-05-27 04:10:15 -0300

    desafio

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -8,27 +8,56 @@ export const useCartContext = () => useContext(CartContext)
 const CartContextProvider = ({children}) =>{
     // Estados y funciones globales
     const [cartList, setCartList] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalItems, setTotalItems] = useState(0);
 
+    
+    function isInCart(id) {
+        return cartList.some(el => el.id === id);
+    }
+    
     function addToCart(item) {
-        setCartList( [
-            ...cartList,
-            item
-        ] )
+        if (isInCart(item.id)) {
+            let i = cartList.findIndex(el => el.id === item.id);
+            const newCartList = cartList;
+            newCartList[i].cantidad += item.cantidad;
+            updateCart(newCartList);
+        } else {
+            updateCart([...cartList,item]);
+        }
     }
 
-    // const eliminarItem =(id)=> {
-
-    // }
+    function updateCart(arr) {
+        setCartList(arr);
+        setTotalPrice(arr
+            .map(curr => curr.cantidad*curr.price)
+            .reduce((acc,curr) => acc+curr,0)
+        );
+        setTotalItems(arr
+            .map(curr => curr.cantidad)
+            .reduce((acc,curr) => acc+curr,0)
+        );
+    }
 
     const vaciarCarrito = () => {
         setCartList([])
     }
 
+    function removeItem(id) {
+        let i = cartList.findIndex(el => el.id === id);
+        const newCartList = cartList;
+        newCartList.splice(i,1);
+        updateCart(newCartList);
+    }    
+
     return (
         <CartContext.Provider value={ {
             cartList,
             addToCart,
-            vaciarCarrito
+            vaciarCarrito,
+            removeItem,
+            totalPrice,
+            totalItems
         } } >
             {children}
         </CartContext.Provider>
