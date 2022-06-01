8f17b1943e286d79111beff9d0f3b840aa53cee3
unknown <sebafantini@yahoo.com>
2022-06-01 02:47:48 -0300

    desafio

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
 
 
 export default function Cart() {
-  const { cartList, vaciarCarrito } = useCartContext()
-  return (
-    
+  const { cartList, vaciarCarrito, removeItem, totalPrice } = useCartContext()
+  
+  if (cartList.length === 0 ) {
+    return (
+        <div>
+            <h3>No hay productos en el Carrito</h3>
+            <Link to='/'>
+                <button>Ir al Menu</button>
+            </Link>
+        </div>
+    );
+  }
+  
+  
+  return (    
     <div>
       <h3>Este es el Carrito</h3>
       <div>
-        {cartList.map(product => <li>{product.title} - price: {product.price} - cantidad: {product.cantidad}</li> )}
+
+        {cartList.map(product => 
+        <li>{product.title} - price: {product.price} - cantidad: {product.cantidad}  <button onClick={removeItem}>Quitar Producto</button> </li> )}        
+        <p>Total: $ {totalPrice}</p>        
         <button onClick={vaciarCarrito}>Vaciar carrito</button>
       </div>    
     </div>
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useCartContext } from '../../context/CartContext'
+
 import './CartWidget.css'
 
 export default function CartWidget() {
+  const { cartList, totalItems } = useCartContext()
   return (    
     <>
       <Link to='/cart'>
-        <img class="cartIcon" src="/cart.png" alt="cart"  ></img>
+        <img class="cartIcon" src="/cart.png" alt="cart"  ></img>        
       </Link>
+      {cartList.length ===0 ? '' : '(' + totalItems + ')'}
     </>
 
   )
