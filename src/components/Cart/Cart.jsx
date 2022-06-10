fac00e9d62620a4f2d42aaf965e476c4dc35aa3b
unknown <sebafantini@yahoo.com>
2022-06-10 02:02:32 -0300

    'desafio'

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
 import { addDoc, collection, doc, documentId, getDocs, getFirestore, query, updateDoc, where, writeBatch } from "firebase/firestore"
 
 export default function Cart() {
   const { cartList, vaciarCarrito, removeItem, totalPrice } = useCartContext()
-  
+  const [idCompraCompleta, setIdCompraCompleta] = useState('')
+
   async function generarOrden() {
 
     let orden = {}     
     
-    orden.buyer = { name: 'Federico', email: 'f@gmail.com', phone: '023456987' }
+    orden.buyer = { name: 'Sebastian', email: 'sebafantini@yahoo.com', phone: '1155014402' }
     orden.total = totalPrice
     orden.date = new Date().toISOString().slice(0, 10)
         
@@ -31,10 +32,15 @@ export default function Cart() {
     // Grabo en Firestore el objeto orden
     const db = getFirestore()
     const queryCollection = collection(db, 'orders')
-    addDoc(queryCollection, orden)
-    .then(resp => console.log(resp))
+    
+    addDoc(queryCollection, orden)    
+    .then(function(resp) {
+      console.log(resp.id)
+      setIdCompraCompleta(resp.id);      
+    })
     .catch(err => console.log(err))
     .finally(()=> vaciarCarrito())
+
 
     
     // Actualizar nivel de stock
@@ -58,11 +64,24 @@ export default function Cart() {
 
   }   
 
-  
+
+
+
   if (cartList.length === 0 ) {
     return (
         <div>
-            <h3>No hay productos en el Carrito</h3>
+               
+
+            { idCompraCompleta ==='' ?                 
+                <h3>No hay productos en el Carrito</h3>  
+                : 
+                <div>
+                  <h3>Gracias por tu compra!</h3>  
+                  <p></p>
+                  <h4>Tu nro. de pedido es: {idCompraCompleta}</h4>
+                  <p></p>
+                </div>                        
+            }            
             <Link to='/'>
                 <button>Ir al Menu</button>
             </Link>
@@ -79,8 +98,8 @@ export default function Cart() {
         {cartList.map(product => 
         <li>{product.title} - price: {product.price} - cantidad: {product.cantidad}  <button onClick={removeItem}>Quitar Producto</button> </li> )}        
         <p>Total: $ {totalPrice}</p>        
-        <button onClick={vaciarCarrito}>Vaciar carrito</button>
-        <button onClick={generarOrden} className='btn btn-outline-danger'>Ralizar compra</button>
+        <button onClick={vaciarCarrito} className='btn btn-outline-danger'>Vaciar carrito</button>
+        <button onClick={generarOrden}  className='btn btn-outline-success'>Ralizar compra</button>
       </div>    
     </div>
   )
